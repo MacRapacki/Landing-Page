7e1e66d9d04773ecbec8eba33d3e949ccae7dfc8
Maciej Rapacki <mac.rapacki@gmail.com>
2020-05-17 01:53:28 +0100

    - first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const burger = document.querySelector(".header__burger")
+const navbar = document.querySelector(".navbar__list")
+const links = document.querySelectorAll(".navbar__link")
+const bars = document.querySelectorAll(".bar")
+
+
+
+burger.addEventListener('click', () => {
+    // toggle navlist
+    navbar.classList.toggle("navbar__list--active");
+
+    //animate links
+    links.forEach((link) => {
+        link.classList.toggle("navbar__link--active")
+    });
+
+    //animate burger
+    bars.forEach((bar) => {
+        bar.classList.toggle("bar--active")
+    })
+})
